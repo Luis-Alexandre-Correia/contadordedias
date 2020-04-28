ce761137b7737938f75963ce40b8adb2328bf57b
Luís Correia <luisalexandrepc@gmail.com>
2020-04-28 09:27:53 -0300

    adicionar contadordedias.py

---
+++ contadordedias.py
@@ -0,0 +1,9 @@
+Quantidade_de_segundos = input("insira a quantidade de segundos: ")
+Segundos_total = int(Quantidade_de_segundos)
+dias =Segundos_total//86400
+horas_restantes = Segundos_total%86400
+horas = horas_restantes//3600
+minutos_restantes = horas_restantes%3600
+minutos = minutos_restantes//60
+segundos= minutos_restantes%60
+print(dias, "Dias,", horas, "Horas,", minutos, "minutos,", segundos, "segundos.")
